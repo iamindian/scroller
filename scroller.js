9292f89987466cce608143a55923481b714bbe3b
Darren Z M LIN <iamindianbbq33@gmail.com>
2017-08-01 18:29:30 +0800

    add wheel event

--- scroller.js
+++ scroller.js
@@ -18,32 +18,50 @@
 		_c.style.overflow = "hidden";
 		_c.style.height = 500 + "px";
 		_c.style.margin = 0 + "px";
-		_c.style.paddingRight = opt.width + "px";
+		document.body.style.overflow = "hidden";
 		var clientTop =  _c.clientTop;
 		var clientLeft = _c.clientLeft;
 		var clientHeight = _c.clientHeight;
 		var clientWidth = _c.clientWidth;
+		var offsetTop = _c.offsetTop;
+		var scrollTop = _c.scrollTop;
 		var offsetHeight = _c.offsetHeight;
 		var offsetWidth = _c.offsetWidth;
 		var scrollHeight = _c.scrollHeight;
-		var scrollVelocity = 3;
+		var scrollVelocity = 10;
+		var maxY,minY;
 		var barHeight;
-		var bar,background;
-		(function init(){
+		var bar,background,display;
+		var offsetParent = _c.offsetParent;
+		if(scrollHeight > clientHeight){
+			init();
+		}
+		function init(){
+			createPlace();
 			calBarHeight();
+			calMinY();
+			calMaxY();
 			createBackground();
 			createScrollBar();
-			var stId = setInterval(function(){
-				var st = _c.scrollTop;
-				if(st==_c.scrollHeight)
-					clearInterval(stId);
-				else
-					_c.scrollTop = _c.scrollTop + 30;
-			},1000);	
-
-		})();
+			initInteract();
+		};
+		function calMinY(){
+			minY = offsetTop + clientTop;	
+		}
+		function calMaxY(){
+			maxY = offsetTop + clientTop + (clientHeight - barHeight); 
+		}
 		function calBarHeight(){
-			barHeight = (clientHeight * scrollVelocity) / (scrollHeight -  clientHeight);	
+			barHeight = clientHeight-(scrollHeight-clientHeight)/scrollVelocity;	
+		}
+		function calMaxScrollTop(){
+			return scrollHeight-clientHeight-barHeight; 	
+		}
+		function calMinScrollTop(){
+			return offsetTop + clientTop	
+		}
+		function createPlace(){
+			_c.style.paddingRight = opt.width + "px";
 		}
 		function createBackground(){
 			background = document.createElement("div");
@@ -56,7 +74,7 @@
 			background.style.float = "left";	
 			background.style.position = "absolute";
 			background.style.paddingTop = clientHeight + "px";
-			_c.offsetParent.appendChild(background);	
+			offsetParent.appendChild(background);	
 		}
 		function createScrollBar(){
 			bar = document.createElement("div");
@@ -69,7 +87,7 @@
 			bar.style.width = opt.width + "px";
 			bar.style.position = "absolute";
 			bar.style.paddingTop = barHeight + "px";	
-			_c.offsetParent.appendChild(bar);
+			offsetParent.appendChild(bar);
 		}
 		function on(){
 			status = true;	
@@ -77,18 +95,38 @@
 		function off(){
 			status = false;	
 		}
-		function mousemove(){
-			//bar.style.top = 0;	
+		function scroll(e){
+			scrollBar(e);
+			e.preventDefault();
+			e.stopPropagation();
+			return false;	
 		}
-		function mousedown(){}
-		function mouseup(){}
-		function trigger(){};
+		function initInteract(){
+			addEventHandler("wheel",scroll);
+		}
 		function addEventHandler(eventName,handler){
-			elem.addEVentListener(eventName,handler);	
+			offsetParent.addEventListener(eventName,(function(){
+				return handler;		
+			})(),false);	
 		}
-		function scroll(val){
-			_c.scrollTop = val;	
+		function scrollBar(e){
+			var current =  bar.offsetTop + e.deltaY;
+			console.log(current);
+			if(current>maxY){
+				bar.style.top = maxY + "px";
+			}else if(current<minY){
+				bar.style.top = minY + "px";
+			}else{
+				bar.style.top = current + "px";
+			}
 		}
+		function scrollContent(e){
+			var current = _c.scrollTop + e.daltaY * scrollVolicity;
+				
+		}
+
+		
+
 		
 
 	}
